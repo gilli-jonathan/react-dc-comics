4935845cda6fb374b61e96fa35c280b631ac8791
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-04 23:52:01 +0100

    fix style in standard footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,29 +27,30 @@ export default function Footer() {
                     </div>
 
                     <div>
-                        <ul> <h3>DC COMICS</h3>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Videos</li>
-                            <li>News</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Videos</li>
-                            <li>News</li>
+                        <ul> <h3>DC</h3>
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscritions</li>
+                            <li>talent Workshop</li>
+                            <li>CPCS Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
                         </ul>
                     </div>
 
                     <div>
-                        <ul> <h3>DC COMICS</h3>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Videos</li>
+                        <ul> <h3>SITES</h3>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>Dc Universe</li>
+                            <li>DC Power Visa</li>
+                            <li>Create By</li>
+                            <li>Jonathan</li>
                         </ul>
                     </div>
                 </nav>
