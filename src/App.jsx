6d0f5aa555669d92f2e9b10c542a2915a6de254f
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-04 18:33:48 +0100

    componets work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Navi from "./components/Navi"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Navi />
     </>
   )
 }
---
+++ src/components/Navi.jsx
@@ -0,0 +1,38 @@
+export default function Navi() {
+
+
+    return (
+        <nav>
+            <ul id="nav-blu" >
+                <li>
+                    <a href="">
+                        DIGITAL COMICS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        dC MERCHANDISE
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        SUBSCRIPTION
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        COMIC SHOP LOCATOR
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        DC POWER VISA
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+
+
+    )
+}
