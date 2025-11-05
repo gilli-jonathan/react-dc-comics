86d3cf9b14bebd4140e526dfc97626b85f61c09f
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-05 09:55:15 +0100

    add icon

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -1,3 +1,9 @@
+import DcPhone from "../assets/img/buy-comics-digital-comics.png"
+import DcTshirt from "../assets/img/buy-comics-merchandise.png"
+// import DcPhone from "../assets/img/buy-comics-digital-comics.png"
+import DcPin from "../assets/img/buy-comics-shop-locator.png"
+// import DcPhone from "../assets/img/buy-comics-digital-comics.png"
+
 export default function Navi() {
 
 
@@ -6,26 +12,31 @@ export default function Navi() {
             <ul className="container">
                 <li>
                     <a href="">
+                        <img src={DcPhone} alt="" />
                         DIGITAL COMICS
                     </a>
                 </li>
                 <li>
                     <a href="">
+                        <img src={DcTshirt} alt="" />
                         DC MERCHANDISE
                     </a>
                 </li>
                 <li>
                     <a href="">
+                        {/* <img src={DcPhone} alt="" /> */}
                         SUBSCRIPTION
                     </a>
                 </li>
                 <li>
                     <a href="">
+                        <img src={DcPin} alt="" />
                         COMIC SHOP LOCATOR
                     </a>
                 </li>
                 <li>
                     <a href="">
+                        {/* <img src={DcPhone} alt="" /> */}
                         DC POWER VISA
                     </a>
                 </li>
